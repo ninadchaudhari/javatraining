0f9464596cce8d98ef7c58b33c6846c0fb5814fc
ninadchaudhari <ninadchaudharioffice@gmail.com>
2022-07-25 15:45:57 +0530

    Added test file

---
+++ src/main/java/com/demo/products/ProductApplicationImpl.java
@@ -0,0 +1,10 @@
+package com.demo.products;
+
+import com.demo.products.exceptions.InvalidInputException;
+
+public class ProductApplicationImpl implements ProductApplication {
+    @Override
+    public String[] searchProduct(String str) throws InvalidInputException {
+        return new String[0];
+    }
+}
---
+++ src/test/java/com/demo/tests/ProductImplTest.java
@@ -0,0 +1,8 @@
+package com.demo.tests;
+
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
+public class ProductImplTest{
+
+}
